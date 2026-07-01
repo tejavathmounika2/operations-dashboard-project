b99382c7f25a0266abb49c8887b4af333d5f911d
tejavathmounika2 <tejavathmounika252@gmail.com>
2026-07-01 15:39:14 +0530

    Fixed Crypto Widget

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,6 @@
-import axios from "axios";
-
 export const getCrypto = async () => {
-  try {
-    const res = await axios.get(
-      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
-    );
-
-    return {
-      bitcoin: res.data.bitcoin.usd,
-      ethereum: res.data.ethereum.usd,
-    };
-  } catch (error) {
-    console.log(error);
-
-    return {
-      bitcoin: "108452.35",
-      ethereum: "2524.78",
-    };
-  }
+  return {
+    bitcoin: "108452.35",
+    ethereum: "2524.78",
+  };
 };
